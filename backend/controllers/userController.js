2aa46345a5398c00cc06361d85c7ccd001e41d01
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-12-26 09:26:36 -0500

    generates json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc Authe user and send back data (toke n to save on client access protect routes)
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
