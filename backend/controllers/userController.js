f6bb1c5f65b1da93396ab791e43cd44ff28badb4
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2021-03-15 12:05:02 -0700

    adds custom suthentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-// @desc Authe user and send back data (toke n to save on client access protect routes)
+// @desc Auth user and send back data (toke n to save on client access protect routes)
 // @route POST /api/users/login 
 // @access Public 
 const authUser = asyncHandler(async (req, res) => {
@@ -24,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+// @desc Get user profile
+// @route GET /api/users/profile 
+// @access Private 
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
