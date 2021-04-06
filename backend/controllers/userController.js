2acf5f213d18aed9fb6d23f7766f57f84266e891
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2021-04-05 19:17:21 -0700

    adds user registration function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-// @desc Auth user and send back data (toke n to save on client access protect routes)
+// @desc Auth user and send back data (token to save on client access protect routes)
 // @route POST /api/users/login 
 // @access Public 
 const authUser = asyncHandler(async (req, res) => {
@@ -21,6 +21,39 @@ const authUser = asyncHandler(async (req, res) => {
   } else {
     res.status(401)
     throw new Error('Invalid email or password.')
+  }
+})
+
+// @desc   Register a new user
+// @route  POST /api/users/ 
+// @access Public 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
 
@@ -43,4 +76,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
